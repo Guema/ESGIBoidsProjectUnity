8463c62d951ab96dc739f42e5d0ee96f4bf1a925
Guema <floorent1993@gmail.com>
2016-02-03 18:47:17 +0100

    Created a quite well working Rule1

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class Boid : Controller
+{
+    [SerializeField]
+    Collider col;
+    List<Boid> boids = new List<Boid>();
+    Vector3 direction;
+
+    void Reset()
+    {
+        col = GetComponent<Collider>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 perceivedMassCenter = new Vector3();
+        foreach (Boid item in boids)
+        {
+            perceivedMassCenter += item.transform.position;
+        }
+        perceivedMassCenter = perceivedMassCenter / boids.Count;
+
+        //DoFirstRule(perceivedMassCenter);
+        DoSecondRule(perceivedMassCenter);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other != col)
+        {
+            boids.Add(other.GetComponent<Boid>());
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Boid b;
+        if (boids.Contains(b = other.GetComponent<Boid>()))
+        {
+            boids.Remove(b);
+        }
+    }
+
+    void DoFirstRule(Vector3 perceivedMassCenter)
+    {
+        if ((transform.position - perceivedMassCenter).magnitude > 0.5f)
+        {
+            
+            direction = (perceivedMassCenter - transform.position).normalized;
+            direction *= unit.GetSpeed();
+            transform.position += direction * Time.deltaTime;
+        }
+    }
+
+    void DoSecondRule(Vector3 perceivedMassCenter)
+    {
+        if ((transform.position - perceivedMassCenter).magnitude > 0.5f)
+        {
+            direction = (perceivedMassCenter - transform.position).normalized;
+        }
+    }
+}
